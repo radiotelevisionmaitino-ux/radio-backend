c24ea7099894e90005e4685ab35a5fad926fc23c
radiotelevisionmaitino-ux <radiotelevisionmaitino@gmail.com>
2026-08-24 01:34:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,24 +7,31 @@ const PORT = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET");
   next();
 });
 
-app.get('/', (req, res) => { res.send('Radio Maitino 24/7 - Servidor Backend Activo'); });
+app.get('/', (req, res) => {
+  res.send('Servidor de Radio Maitino Activo. Escucha en /live');
+});
 
 app.get('/live', (req, res) => {
   res.writeHead(200, {
     'Content-Type': 'audio/mpeg',
     'Transfer-Encoding': 'chunked',
     'Connection': 'keep-alive',
-    'Cache-Control': 'no-cache, no-store, must-revalidate'
+    'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+    'Pragma': 'no-cache',
+    'Expires': '0',
+    'X-Content-Type-Options': 'nosniff'
   });
+  
   addClient(res);
   req.on('close', () => removeClient(res));
 });
 
 app.listen(PORT, () => {
-  console.log(`[SERVIDOR] Radio Maitino en el puerto ${PORT}`);
+  console.log(`[SERVIDOR] Iniciado en el puerto ${PORT}`);
   initScheduleManager();
   startEngine();
 });
